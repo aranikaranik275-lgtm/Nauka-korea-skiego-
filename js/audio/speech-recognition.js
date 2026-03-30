86f427cdef395b785ec09a08f7e8750ccc9fd89e
aranikaranik275-lgtm <aranikaranik275-lgtm@users.noreply.github.com>
2026-03-30 08:43:26 +0000

    deploy: 3e17a75e033394120b5788aa1c8f2fae58ce52f3

--- js/audio/speech-recognition.js
+++ js/audio/speech-recognition.js
@@ -1,1 +1,35 @@
 
+export class SpeechRecognitionManager {
+  constructor() {
+    this.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    this.supported = !!this.SpeechRecognition;
+    this.recognition = null;
+  }
+
+  init() {
+    if (!this.supported) return;
+    this.recognition = new this.SpeechRecognition();
+    this.recognition.lang = 'ko-KR';
+    this.recognition.interimResults = false;
+    this.recognition.maxAlternatives = 1;
+  }
+
+  start(callback) {
+    if (!this.supported) {
+      alert('Rozpoznawanie mowy nie jest obsługiwane w tej przeglądarce.');
+      return;
+    }
+    if (!this.recognition) this.init();
+    this.recognition.onresult = event => {
+      const transcript = event.results[0][0].transcript;
+      callback(transcript);
+    };
+    this.recognition.onerror = event => {
+      [span_5](start_span)if (event.error === 'not-allowed') alert('Brak zgody na mikrofon.');[span_5](end_span)
+    };
+    this.recognition.onend = () => {
+      this.recognition = new this.SpeechRecognition();
+    };
+    this.recognition.start();
+  }
+}

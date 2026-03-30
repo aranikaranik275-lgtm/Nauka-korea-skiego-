5ab2a2869f46b3c79d2510ffae0a0b21610a2a99
aranikaranik275-lgtm <aranikaranik275@gmail.com>
2026-03-30 10:44:31 +0200

    Update audio-manager.js

--- js/audio/audio-manager.js
+++ js/audio/audio-manager.js
@@ -1,1 +1,18 @@
+import { TTS } from './tts.js';
+import { SpeechRecognitionManager } from './speech-recognition.js';
+
+export class AudioManager {
+  constructor() {
+    this.tts = new TTS();
+    this.speech = new SpeechRecognitionManager();
+  }
+
+  async init() {
+    await this.tts.init();
+    this.speech.init();
+  }
+
+  speak(text) { this.tts.speak(text); }
+  listen(callback) { this.speech.start(callback); }
+}
 
